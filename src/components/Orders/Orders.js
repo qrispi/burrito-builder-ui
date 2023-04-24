6af22feb32e03091ca3bf65e4794e636eca946bf
Christopher Baum <christopherabaum@gmail.com>
2023-04-24 12:50:45 -0500

    Add deleteOrder function and button to Orders

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Orders.css';
 
-const Orders = props => {
-  const orderEls = props.orders.map((order, index) => {
+const Orders = ({orders, deleteOrder}) => {
+  const orderEls = orders.map((order, index) => {
     return (
       <div className="order" key={index}>
         <h3>{order.name}</h3>
@@ -11,6 +11,7 @@ const Orders = props => {
             return <li key={index}>{ingredient}</li>
           })}
         </ul>
+        <button onClick={() => deleteOrder(order.id)}>Complete Order</button>
       </div>
     )
   });
