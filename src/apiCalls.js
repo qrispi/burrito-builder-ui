189325ddbbf21a5ba64210f51c0df5817db467da
Christopher Baum <christopherabaum@gmail.com>
2023-04-24 11:32:17 -0500

    Add POST api call function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,11 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json());
 }
+
+export const postOrder = (order) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    body: JSON.stringify(order),
+    headers: { 'Content-Type': 'application/json'}
+  }).then(response => response.json());
+}
