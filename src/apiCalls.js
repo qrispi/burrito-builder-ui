42a2f40b370a415cfe03cb47d6099bb42f63dc16
Christopher Baum <christopherabaum@gmail.com>
2023-04-24 12:58:51 -0500

    Add delete fetch request call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -10,3 +10,10 @@ export const postOrder = (order) => {
     headers: { 'Content-Type': 'application/json'}
   }).then(response => response.json());
 }
+
+export const deleteOrder = (id) => {
+  fetch('http://localhost:3001/api/v1/orders/' + id, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json'}
+  });
+}
