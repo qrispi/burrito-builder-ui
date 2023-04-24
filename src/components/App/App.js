61efa23755b2b75453639fa87c501726d4233990
Christopher Baum <christopherabaum@gmail.com>
2023-04-24 11:32:45 -0500

    Add addOrder function to App and pass to OrderForm

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import {getOrders, postOrder} from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 import { useState, useEffect } from 'react';
@@ -14,11 +14,16 @@ function App () {
     promise.then(data => setOrders(data.orders));
   }, []);
 
+  const addOrder = (newOrder) => {
+    const promise = postOrder(newOrder);
+    promise.then(data => console.log(data));
+  }
+
   return (
     <main className="App">
       <header>
         <h1>Burrito Builder</h1>
-        <OrderForm />
+        <OrderForm addOrder={addOrder}/>
       </header>
 
       <Orders orders={orders}/>
