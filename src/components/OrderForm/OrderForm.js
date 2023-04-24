31dfc16bcfe6148a31517d8e812c7622767b5f68
Christopher Baum <christopherabaum@gmail.com>
2023-04-24 11:08:43 -0500

    Add state to control disabled state for ingredient buttons

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -3,10 +3,18 @@ import { useState } from 'react';
 
 function OrderForm () {
 
+  const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
   const [formInputs, setFormInputs] = useState({
     name: '',
     ingredients: []
-  })
+  });
+
+  const [disabled, setDisabled] = useState(
+    possibleIngredients.reduce((acc, ingred) => {
+      acc[ingred] = false
+      return acc
+    }, {})
+  )
 
 
   const handleSubmit = e => {
@@ -26,12 +34,12 @@ function OrderForm () {
   const handleIngredientChange = e => {
     e.preventDefault();
     setFormInputs({...formInputs, ingredients: [...formInputs.ingredients, e.target.name]});
+    setDisabled({...disabled, [e.target.name]: true})
   }
 
-  const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
   const ingredientButtons = possibleIngredients.map(ingredient => {
     return (
-      <button key={ingredient} name={ingredient} onClick={e => handleIngredientChange(e)}>
+      <button key={ingredient} name={ingredient} onClick={e => handleIngredientChange(e)} disabled={disabled[ingredient]}>
         {ingredient}
       </button>
     )
@@ -57,7 +65,9 @@ function OrderForm () {
 
       <button onClick={(e) => {
         e.preventDefault();
-        console.log(formInputs)}}>
+        console.log(formInputs)
+        console.log(disabled)
+        }}>
         Check State
       </button>
     </form>
